dee8e89191ee662db4388be237b19ef2974c6ce3
Oerjan Hoel <orjan.hoel@gmail.com>
2025-10-06 18:52:42 -0700

    Added authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,37 @@
 "use client";
-import React, { useEffect } from "react";
-import Link from "next/link";
+import React, { useEffect } from "react"; // Import useEffect form React
+import Link from "next/link"; // Import link from next/Link 
+// Import needed packages for auth
 import {
   signInWithGoogle,
   signOut,
   onIdTokenChanged,
 } from "@/src/lib/firebase/auth.js";
-import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js";
-import { setCookie, deleteCookie } from "cookies-next";
+import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js"; // Import of filler from firestore.js
+import { setCookie, deleteCookie } from "cookies-next"; // Import of Cookie functions from cookies-next
 
+// Initiate the Cookies for the user authentication
 function useUserSession(initialUser) {
-  return;
+  useEffect(() => {
+    // Update the __session cookies
+    return onIdTokenChanged(async (user) => {
+      if (user) {
+        const idToken = await user.getIdToken();
+        await setCookie("__session", idToken);
+      } else {
+        await deleteCookie("__session");
+      }
+      if (initialUser?.uid === user?.uid) {
+        return;
+      }
+      window.location.reload();
+    });
+  }, [initialUser]);
+
+  return initialUser;
 }
 
+// Export the function Header to files that needs it
 export default function Header({ initialUser }) {
   const user = useUserSession(initialUser);
 
@@ -25,7 +44,7 @@ export default function Header({ initialUser }) {
     event.preventDefault();
     signInWithGoogle();
   };
-
+  // Returns JSX code displaying user info
   return (
     <header>
       <Link href="/" className="logo">
--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -2,13 +2,27 @@
 // https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#keeping-server-only-code-out-of-the-client-environment
 import "server-only";
 
-import { cookies } from "next/headers";
-import { initializeServerApp, initializeApp } from "firebase/app";
+import { cookies } from "next/headers"; // Import cookies from next/headers package
+import { initializeServerApp, initializeApp } from "firebase/app"; // Imports the app initilization
 
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"; // Imports the authentication function from auth.js
 
 // Returns an authenticated client SDK instance for use in Server Side Rendering
 // and Static Site Generation
 export async function getAuthenticatedAppForUser() {
-  throw new Error("not implemented");
+  const authIdToken = (await cookies()).get("__session")?.value;
+
+  // Variable for app initilization
+  const firebaseServerApp = initializeServerApp(
+    // https://github.com/firebase/firebase-js-sdk/issues/8863#issuecomment-2751401913
+    initializeApp(),
+    {
+      authIdToken,
+    }
+  );
+  // Variable holding authentication token
+  const auth = getAuth(firebaseServerApp);
+  await auth.authStateReady();
+
+  return { firebaseServerApp, currentUser: auth.currentUser };
 }
